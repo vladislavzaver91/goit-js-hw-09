b1b05251d31766af6012379a66b27ae9b351f92f
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2022-09-28 18:01:11 +0300

    add solution to task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+let timerId = null;
+
+startBtnRef.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+    changesStartBtnStatus();
+});
+
+stopBtnRef.addEventListener('click', () => {
+    clearInterval(timerId);
+    changesStopBtnStatus();
+});
+
+function changesStartBtnStatus() {
+    startBtnRef.disabled = true;
+    stopBtnRef.disabled = false;
+};
+
+function changesStopBtnStatus() {
+    stopBtnRef.disabled = true;
+    startBtnRef.disabled = false;
+};
