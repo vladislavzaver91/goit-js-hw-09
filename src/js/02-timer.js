ecde3650a4ebece795a66c74aafba003a4468037
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2022-10-01 11:44:38 +0300

    add solution to task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -27,7 +27,8 @@ flatpickr(datetimeRef, options);
 
 const timer = {
     intervalId: null,
-    refs:{
+    isActive: false,
+    refs: {
         daysRef: document.querySelector('[data-days]'),
         hoursRef: document.querySelector('[data-hours]'),
         minutesRef: document.querySelector('[data-minutes]'),
@@ -35,45 +36,51 @@ const timer = {
     },
 
     start() {
+        if(this.isActive) {
+            return;
+        }
+
         timerDeadline = new Date();
+        this.isActive = true;
+
         this.intervalId = setInterval(() => {
-            const delta = timerDeadline - Date.now();
-            const timeComponents = convertMs(delta);
-            const { days, hours, minutes, seconds } = this.refs;
-            this.refs.daysRef.textContent = addLeadingZero(timeComponents.days);
-            this.refs.hoursRef.textContent = addLeadingZero(timeComponents.hours);
-            this.refs.minutesRef.textContent = addLeadingZero(timeComponents.minutes);
-            this.refs.secondsRef.textContent = addLeadingZero(timeComponents.seconds);
-            // console.log(timeComponents);
-            console.log(delta);
-            console.log(timeComponents);
+            const currentTime = Date.now();
+            const delta = timerDeadline - currentTime;
+
+            // if (delta <= 0) {
+            //     clearInterval(this.intervalId);
+            // }
+
+            const { days, hours, minutes, seconds } = this.convertMs(delta);
+            this.refs.daysRef.textContent = this.addLeadingZero(days);
+            this.refs.hoursRef.textContent = this.addLeadingZero(hours);
+            this.refs.minutesRef.textContent = this.addLeadingZero(minutes);
+            this.refs.secondsRef.textContent = this.addLeadingZero(seconds);
         }, 1000)
     },
 
+    convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
     
+        // Remaining days
+        const days = Math.floor(ms / day);
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    
+        return { days, hours, minutes, seconds };
+    },
+
+    addLeadingZero(value) {
+        return String(value).padStart(2, '0');
+    },
 }
+startBtnRef.addEventListener('click', timer.start.bind(timer));
 
-timer.start();
-
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-};
-
-function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-};
